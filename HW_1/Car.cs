5d6c7822897468fea5bb9ee0ed738f0cf9b51e7e
oydzhi <bachurinvv54@gmail.com>
2026-04-08 15:49:28 +0500

    HW_1

---
+++ HW_1/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Car
+{
+    private string brand;
+    private int speed;
+    private double fuel;
+
+    public string Brand { get { return brand; } }
+    public int Speed { set { speed = value; } }
+    public double Fuel { get { return fuel; } set { fuel = value; } }
+
+    // конструкторы
+    public Car()
+    {
+        brand = "Unknown";
+        speed = 0;
+        fuel = 0;
+    }
+
+    public Car(string brand, int speed, double fuel)
+    {
+        this.brand = brand;
+        this.speed = speed;
+        this.fuel = fuel;
+    }
+
+    public Car(Car other)
+    {
+        this.brand = other.brand;
+        this.speed = other.speed;
+        this.fuel = other.fuel;
+    }
+
+    // методы с выводом
+    public void Drive()
+    {
+        Console.WriteLine($"{brand} едет со скоростью {speed} км/ч");
+    }
+
+    public void Drive(int newSpeed)
+    {
+        speed = newSpeed;
+        Console.WriteLine($"{brand} теперь едет со скоростью {speed} км/ч");
+    }
+
+    public void Refuel(double amount)
+    {
+        fuel += amount;
+        Console.WriteLine($"{brand} заправлено {amount} литров. Топливо сейчас: {fuel}");
+    }
+
+    public void Refuel()
+    {
+        fuel += 10;
+        Console.WriteLine($"{brand} автозаправка +10 литров. Топливо сейчас: {fuel}");
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Марка: {brand}, Скорость: {speed}, Топливо: {fuel}");
+    }
+
+    public void ShowInfo(string msg)
+    {
+        Console.WriteLine(msg);
+        ShowInfo();
+    }
+}
---
+++ HW_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+class Program
+{
+    static void Main()
+    {
+        // конструктор по умолчанию
+        Car car1 = new Car();
+        car1.Speed = 80;
+        car1.Fuel = 30;
+        car1.ShowInfo();
+
+        Console.WriteLine();
+
+        // конструктор с параметрами
+        Car car2 = new Car("Audi", 120, 40);
+        car2.Drive();
+        car2.Refuel(15);
+        car2.ShowInfo("Вторая машина:");
+
+        Console.WriteLine();
+
+        // конструктор копирования
+        Car car3 = new Car(car2);
+        car3.Drive(150);
+        car3.Refuel();
+        car3.ShowInfo();
+
+        Console.WriteLine("\nНажмите Enter для выхода...");
+        Console.ReadLine(); // чтобы консоль не закрывалась
+    }
+}
