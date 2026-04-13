da45f31acfa566f165d6a26571552d7d3c3d58ce
personaive <nik.bachurin2704@mail.ru>
2026-04-13 10:49:19 +0500

    Update Car.cs

--- HW_1/Car.cs
+++ HW_1/Car.cs
@@ -1,47 +1,63 @@
-﻿using System;
+using System;
 
 public class Car
 {
+    // поля
     private string brand;
     private int speed;
     private double fuel;
-
-    public string Brand { get { return brand; } }
-    public int Speed { set { speed = value; } }
-    public double Fuel { get { return fuel; } set { fuel = value; } }
-
-    // конструкторы
+    
+    // конструкторы: по умолчанию
     public Car()
     {
         brand = "Unknown";
         speed = 0;
         fuel = 0;
     }
-
+    
+    // с параметрами
     public Car(string brand, int speed, double fuel)
     {
         this.brand = brand;
         this.speed = speed;
         this.fuel = fuel;
     }
-
-    public Car(Car other)
+    
+    // копирования
+    public Car(Car car)
     {
-        this.brand = other.brand;
-        this.speed = other.speed;
-        this.fuel = other.fuel;
+        this.brand = car.brand;
+        this.speed = car.speed;
+        this.fuel = car.fuel;
     }
 
+    // свойства
+    public string Brand
+    {
+        get { return brand; }
+    }
+
+    public int Speed
+    {
+        set { speed = value; }
+    }
+
+    public double Fuel
+    {
+        get { return fuel; }
+        set { fuel = value; }
+    }
+    
     // методы с выводом
     public void Drive()
     {
-        Console.WriteLine($"{brand} едет со скоростью {speed} км/ч");
+        Console.WriteLine($"{brand} едет {speed} км/ч");
     }
 
     public void Drive(int newSpeed)
     {
         speed = newSpeed;
-        Console.WriteLine($"{brand} теперь едет со скоростью {speed} км/ч");
+        Console.WriteLine($"{brand} теперь едет {speed} км/ч");
     }
 
     public void Refuel(double amount)
