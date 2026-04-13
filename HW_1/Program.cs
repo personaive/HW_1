230b4742fb6a35d2a7e74a3925e58aa643da32a2
personaive <nik.bachurin2704@mail.ru>
2026-04-13 10:49:04 +0500

    Update Program.cs

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,33 +1,27 @@
-﻿using System;
-using System.Runtime.ConstrainedExecution;
+using System;
 
 class Program
 {
     static void Main()
     {
-        // конструктор по умолчанию
+        // по умолчанию
         Car car1 = new Car();
         car1.Speed = 80;
         car1.Fuel = 30;
         car1.ShowInfo();
-
         Console.WriteLine();
 
-        // конструктор с параметрами
-        Car car2 = new Car("Audi", 120, 40);
+        // с параметрами
+        Car car2 = new Car("BMW", 120, 40);
         car2.Drive();
         car2.Refuel(15);
-        car2.ShowInfo("Вторая машина:");
-
+        car2.ShowInfo();
         Console.WriteLine();
 
-        // конструктор копирования
+        // копирования
         Car car3 = new Car(car2);
         car3.Drive(150);
         car3.Refuel();
         car3.ShowInfo();
-
-        Console.WriteLine("\nНажмите Enter для выхода...");
-        Console.ReadLine(); // чтобы консоль не закрывалась
     }
 }
